9f81c247bf9541a62d7633c431c803d4cc20cef1
Bautista Sobenko <bautipriano@gmail.com>
2023-10-16 16:36:37 -0300

    validacion de datos - ascensores

--- gestion/ascensores/index.js
+++ gestion/ascensores/index.js
@@ -15,21 +15,30 @@ const obtenerAscensor = (id) => {
     return ascensorObtenido !== undefined ? ascensorObtenido : console.log(`No existe un ascensor con id ${id}`);;
 }
 
+const validarDatos = (pisos, nombre, estado) => {
+    
+    if (pisos === 0) {
+        console.log('El ascensor debe tener al menos un piso');
+        return false;
+    }
+    if (validarVacio(nombre)) {
+        console.log('El ascensor debe tener un nombre');
+        return false;
+    }
+    if (validarVacio(estado)) {
+        console.log('Los pisos deben tener un nombre');
+        return false;
+    }
+
+    return true;
+
+}
+
 const crearAscensor = (ascensor) => {
     ascensor.id = randomUUID();
 
-    //TODO: Crear metodo que valide los datos del ascensor
-    if (ascensor.pisos.length === 0) {
-        console.log('El ascensor debe tener al menos un piso');
-        return;
-    }
-    if (validarVacio(ascensor.nombre)) {
-        console.log('El ascensor debe tener un nombre');
-        return;
-    }
-    if (validarVacio(ascensor.estado)) {
-        console.log('Los pisos deben tener un nombre');
-        return;
+    if( !validarDatos(ascensor.pisos.length, ascensor.nombre, ascensor.estado) ){
+        return 0;
     }
 
     const ascensores = obtenerAscensores();
@@ -46,17 +55,8 @@ const actualizarAscensor = (id, ascensor) => {
         return;
     }
 
-    if (ascensor.pisos.length === 0) {
-        console.log('El ascensor debe tener al menos un piso');
-        return;
-    }
-    if (validarVacio(ascensor.nombre)) {
-        console.log('El ascensor debe tener un nombre');
-        return;
-    }
-    if (validarVacio(ascensor.estado)) {
-        console.log('Los pisos deben tener un nombre');
-        return;
+    if( !validarDatos(ascensor.pisos.length, ascensor.nombre, ascensor.estado) ){
+        return 0;
     }
 
     ascensores[index] = ascensor;
