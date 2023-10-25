a8a83b8c1739751b7564b3ca88d3be873213f029
Bautista Sobenko <bautipriano@gmail.com>
2023-10-25 01:19:51 -0300

    Avance Gestion visitantes y Gestion permisos

--- cliente-web/js/admin.js
+++ cliente-web/js/admin.js
@@ -3,7 +3,7 @@
 (function () {
     function init() {
         var router = new Router([
-            new Route('login', 'login.html', true)     
+            new Route('ascensores', 'gestion-ascensores.html', true)           
         ]);
     }
     init();
---
+++ cliente-web/js/gestion-visitantes.mjs
@@ -0,0 +1,75 @@
+import visitanes from "../../gestion/persistencia/guests.json" assert {type: "json"};
+const cuerpoTabla = document.querySelector('#cuerpo-tabla');
+
+const cargarTabla = () => {
+
+    //GET (devuelve ascensores.json)
+
+    cuerpoTabla.innerHTML = '';
+
+    visitanes.map( (vis) => {
+    
+        const fila = document.createElement('tr');
+        
+        const celdas = `<td>${vis.id}</td>
+                        <td>${vis.nombre}</td>
+                        <td>${vis.edad}</td>
+                        <td>${vis.email}</td>
+                        <td>${vis.pisos}</td>
+                        <td>${vis.fecha_checkIn}</td>
+                        <td>${vis.fecha_checkOut}</td>
+                        <td class="table-actions">
+                            <div>
+                                <button id="btn-edit">
+                                    <i class="fa-solid fa-pencil"></i>
+                                </button>
+                            </div>
+                            <div>
+                                <button id="btn-remove" onclick="eliminarVisitante('${vis.id}')">
+                                    <i class="fa-regular fa-circle-xmark"></i>
+                                </button>
+                            </div>
+                        </td>`;
+
+        fila.innerHTML = celdas;
+        cuerpoTabla.append(fila);
+    } )
+    
+};
+
+const agregarVisitante = () => {
+
+    let nombre = document.querySelector('#nombre').value;
+    let edad = document.querySelector('#edad').value;
+    let email = document.querySelector('#email').value;
+    let strPisos = document.querySelector('#pisos').value;
+    let pisos_permitidos = strPisos.split(',');
+    
+    //(?) Fecha checkIn, checkOut 
+
+    //POST
+    let visitante = {
+        id: null,
+        nombre,
+        edad,
+        email, 
+        pisos_permitidos, 
+    };
+
+    //Realizo un GET y actualizo la tabla?
+    
+}
+
+window.eliminarVisitante = (id) => {
+
+    console.log(id);
+
+    //DELETE 
+
+    //Realizo un GET y actualizo la tabla?
+
+}
+
+cargarTabla();
+
+document.querySelector('#formVisitante').addEventListener('submit', agregarVisitante);
